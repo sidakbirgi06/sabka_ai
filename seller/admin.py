ce846f6ecc4467d2ec69d724fc9e684219291d76
Ridak33 <ridakbirgi3@gmail.com>
2025-08-23 22:31:32 +0530

    Reorder social media field in BusinessProfile model

--- seller/admin.py
+++ seller/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import BusinessProfile, ChatbotSettings # Import our models
+from .models import BusinessProfile, ChatbotSettings, FacebookPage # Import our models
 
 # Register your models here.
 # This tells Django to show these models in the admin site.
 admin.site.register(BusinessProfile)
 admin.site.register(ChatbotSettings)
+admin.site.register(FacebookPage)
